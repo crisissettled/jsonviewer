267e9b666c79aa34d45254490f47220af2a9cb3f
James Liu <jliu@sirokygroup.com>
2023-12-30 16:38:17 -0500

    using monaco-editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,70 @@
-import { useState } from "react";
-import "./App.css";
+import Editor from "@monaco-editor/react";
+import { useState, useRef, useEffect } from "react";
 
-function App() {
-  const [jsonText, setJsonText] = useState("");
-  const [errorMsg, setErrorMsg] = useState("");
+const formatTypes = [
+  { type: "json", name: "JSON" },
+  { type: "html", name: "HTML" },
+  { type: "javascript", name: "JAVASCRIPT" },
+];
 
-  const handleJsonClear = () => {
-    setJsonText("");
+export default function App() {
+  const [code, setCode] = useState("");
+  const [curType, setCurType] = useState("json");
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (code && editorRef) {
+      editorRef.current.getAction("editor.action.formatDocument").run();
+    }
+  }, [curType]);
+
+  const handleEditorDidMount = (editor) => {
+    // "monacoRef" was instantiated using React.useRef()
+    editorRef.current = editor;
+    // this varies from my answer in StackOverflow for some reason...
+    setTimeout(() => {
+      editorRef.current.getAction("editor.action.formatDocument").run();
+    }, 1000);
   };
 
-  const handleJsonFormat = () => {
-    if (!jsonText) return;
-    try {
-      const formattedJsonText = JSON.stringify(JSON.parse(jsonText), null, 2);
-      setJsonText(formattedJsonText);
-    } catch (err) {
-      setErrorMsg(err.message);
-    }
+  const handleTypeChangeClick = (type) => {
+    console.log(type, "new type -----");
+    setCurType(type);
+  };
+
+  const handleFormatClick = () => {
+    editorRef.current.getAction("editor.action.formatDocument").run();
   };
 
   return (
-    <>
-      <div className="menu">
-        <div className="title">JSON Viewer</div>
-        <button className="btn btn-clear" onClick={handleJsonClear}>
-          Clear
-        </button>
-        <button className="btn btn-format" onClick={handleJsonFormat}>
-          Format
-        </button>
-        <div className="error-msg">{errorMsg}</div>
+    <div>
+      <div>
+        <select
+          onChange={(e) => handleTypeChangeClick(e.target.value)}
+          style={{ height: "2rem", width: "12rem" }}
+        >
+          {formatTypes.map((ele, index) => (
+            <option key={index} value={ele.type}>
+              {ele.name}
+            </option>
+          ))}
+        </select>
       </div>
-      <textarea
-        value={jsonText}
-        className="json-text"
-        onChange={(e) => {
-          setJsonText(e.target.value);
-          setErrorMsg("");
+      <div>
+        <button onClick={() => setCode("")}>Clear</button>
+        <button onClick={() => handleFormatClick()}>Format</button>
+      </div>
+      <Editor
+        value={code}
+        height="700px"
+        theme="vs-dark"
+        options={{
+          formatOnPaste: true,
         }}
-      ></textarea>
-    </>
+        language={curType}
+        onChange={(value) => setCode(value)}
+        onMount={handleEditorDidMount}
+      />
+    </div>
   );
 }
-
-export default App;
